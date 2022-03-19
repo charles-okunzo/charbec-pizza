bf0025f8b6e9c34d2cb822ebf6f8bf6ebe26fbd1
charles okunzo <okunzopeter01@gmail.com>
2022-03-19 14:01:12 +0300

    feature:slideshow-enable slideshow feature

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+let i=0;
+let images=[];
+const time=3000;
+images[0]='assets/pizza-01.jpg';
+images[1]='assets/pizza-02.jpg';
+images[2]='assets/pizza-03.jpg';
+images[3]='assets/pizza-04.jpg';
+images[4]='assets/pizza-05.jpg';
+images[5]='assets/pizza-06.jpg';
+images[6]='assets/pizza-08.jpg';
+images[7]='assets/pizza-09.jpg';
+images[8]='assets/pizza-10.jpg';
+images[9]='assets/pizza-14.jpg';
+
+function mySlideshow(){
+
+  document.querySelector('img#img-tag').src= images[i];
+  if(i<images.length-1){
+    i++;
+  }else{
+    i=0;
+  }
+
+  setTimeout("mySlideshow()", time);
+};
+
+window.onload=mySlideshow();
